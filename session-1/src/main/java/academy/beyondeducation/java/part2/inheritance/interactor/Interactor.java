ce53f5bd4fe149186737ece107ec7c79d6f9e4c2
Guillermo Herrero <guillermo.herrero.work@gmail.com>
2023-09-12 07:44:43 +0200

    Refactor Interactor

--- session-1/src/main/java/academy/beyondeducation/java/part2/inheritance/interactor/Interactor.java
+++ session-1/src/main/java/academy/beyondeducation/java/part2/inheritance/interactor/Interactor.java
@@ -1,7 +1,7 @@
 package academy.beyondeducation.java.part2.inheritance.interactor;
 
 public class Interactor {
-    protected String formatInteraction(String greeting, String person) {
-        return greeting + ", " + person + "!";
+    protected String formatInteraction(String message, String person) {
+        return message + ", " + person + "!";
     }
 }
