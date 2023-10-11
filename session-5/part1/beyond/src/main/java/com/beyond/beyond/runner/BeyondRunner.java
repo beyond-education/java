65b1cf229c8b144446a1459a1caf870edbde8f14
Guillermo Herrero <guillermo.herrero.work@gmail.com>
2023-10-11 08:52:23 +0200

    Add session 5 - application runner

---
+++ session-5/part1/beyond/src/main/java/com/beyond/beyond/runner/BeyondRunner.java
@@ -0,0 +1,13 @@
+package com.beyond.beyond.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeyondRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Beyond Coding.");
+    }
+}
