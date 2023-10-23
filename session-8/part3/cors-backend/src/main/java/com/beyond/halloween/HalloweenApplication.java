161c61075da3039a36e90eab4d5cf976f42ccb55
Guillermo Herrero <guillermo.herrero.work@gmail.com>
2023-10-23 11:54:30 +0200

    Add session 8 part 3 halloween

---
+++ session-8/part3/cors-backend/src/main/java/com/beyond/halloween/HalloweenApplication.java
@@ -0,0 +1,13 @@
+package com.beyond.halloween;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalloweenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalloweenApplication.class, args);
+	}
+
+}
